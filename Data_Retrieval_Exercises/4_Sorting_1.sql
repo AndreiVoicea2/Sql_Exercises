f6234b947bd1d88f12110a83b3fd04a05f61ec65
AndreiVoicea2 <axel26alex@yahoo.com>
2023-10-29 17:43:51 +0200

    Upload Exercise 7

---
+++ Data_Retrieval_Exercises/4_Sorting_1.sql
@@ -0,0 +1,4 @@
+-- select * from employees order by officeCode;
+-- select * from employees order by firstName desc;
+-- select * from employees where jobTitle = 'Sales Rep' order by lastName desc;
+select * from customers order by country, state asc;
