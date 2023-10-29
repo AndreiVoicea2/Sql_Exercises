efdd0d62354aac21b4625ef631d76f4e5223c1e2
AndreiVoicea2 <axel26alex@yahoo.com>
2023-10-29 15:27:14 +0200

    Upload Exercise 5

---
+++ Data_Retrieval_Exercises/2_DataFiltrationScript.sql_4.sql
@@ -0,0 +1,6 @@
+-- select officeCode from employees;
+-- select distinct officeCode from employees;
+-- select jobTitle from employees;
+-- select distinct jobTitle from employees;
+-- describe employees;
+desc employees;
