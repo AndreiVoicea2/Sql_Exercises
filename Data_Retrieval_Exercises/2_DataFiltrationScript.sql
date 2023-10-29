0a00cbeef3272c5fb1a7a542dba08340aec4bb13
AndreiVoicea2 <axel26alex@yahoo.com>
2023-10-29 13:38:36 +0200

    Upload Exercise 2

---
+++ Data_Retrieval_Exercises/2_DataFiltrationScript.sql
@@ -0,0 +1,7 @@
+-- select * from employees where employeeNumber=1286;
+-- select * from employees where officeCode = 6;
+-- select * from employees where lastName = 'King';
+-- select * from employees where jobTitle = 'Sales Rep';
+-- select lastName, firstName, email from employees where jobTitle = 'Sales Rep';
+-- select * from employees where employeeNumber>=1216;
+ select * from employees where lastName != 'King';
