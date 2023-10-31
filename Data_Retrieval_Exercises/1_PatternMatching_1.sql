c6082f06aae51cc660bcd892e5e9412a5983de61
AndreiVoicea2 <axel26alex@yahoo.com>
2023-10-31 19:47:01 +0200

    UploadExercise8

---
+++ Data_Retrieval_Exercises/1_PatternMatching_1.sql
@@ -0,0 +1,10 @@
+-- select * from employees where firstName = 'Jeff';
+-- select * from employees where firstName like 'A%';
+-- select * from employees where lastName like 'p%';
+-- select * from customers where customerName like '%co.';
+-- select * from customers where customerName like 'c%co.';
+-- select * from customers where addressLine1 like '%56%';
+-- select * from customers where customerName like '_A%';
+-- select * from customers where customerName like 'C%o_';
+-- select * from customers where contactFirstName like '_e%e_';
+select * from customers where contactFirstName like '_____';
